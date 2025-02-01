44fa57f93efac02e5f6fcf4b3c57ab1ead9e4f68
adwaith027 <adwaithh227@gmail.com>
2025-02-02 01:32:11 +0530

    crud

--- medstoreproject/medstoreapp/views.py
+++ medstoreproject/medstoreapp/views.py
@@ -1,41 +1,61 @@
 from django.shortcuts import render,redirect
 from .models import storeModel
 from .forms import storeForm
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required,user_passes_test
+
+def password_validation(password):
+    if len(password)<8:
+          return False
+    return True
 
 def signup(request):
     if request.method=='POST':
-         form=UserCreationForm(request.POST)
-         if form.is_valid():
-              form.save()
-              return redirect('login')
-    form=UserCreationForm()
-    return render(request,'signup.html',{'form':form})
+        uname=request.POST.get('username')
+        uemail=request.POST.get('email')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+        if password1 != password2:
+            messages.error(request,'Passwords do not match')
+            return redirect('signup')
+        pwd=password2
+        if not password_validation(pwd):
+            messages.error(request,'Password must be atleast 8 characters long')
+            return redirect('signup')
+        user=User.objects.create_user(username=uname,email=uemail,password=pwd)
+        return redirect('login')
+    return render(request,'signup.html')
 
-def login(request):
+def userlogin(request):
     if request.method=='POST':
-         form=AuthenticationForm(request.POST)
-         if form.is_valid():
-              user=form.get_user()
-              username=user.username
+         uname=request.POST.get('username')
+         password=request.POST.get('password')
+         user=authenticate(username=uname,password=password)
+         if user is not None:
               login(request,user)
-              return redirect('home',{'user':username})
-    form=AuthenticationForm()
-    return render(request,'login.html',{'form':form})
+              return redirect('home')
+    return render(request,'login.html')
 
-def logout(request):
+@login_required(login_url='login')
+def userlogout(request):
     if request.method=='POST':
           logout(request)
           return redirect('login')
     user=request.user
     return render(request,'logout.html',{'user':user})
 
+@login_required(login_url='login')
+def home(request):
+     return render(request,'home.html')
 
+@login_required(login_url='login/')
+def aboutus(request):
+     return render(request,'about.html')
+
+@login_required(login_url='login')
 def create(request):
     form=storeForm()
     if request.method=='POST':
@@ -52,3 +72,25 @@ def create(request):
             
             return redirect('create')
     return render(request,'create.html',{'form':form,"error":""})
+
+def viewall(request):
+     medicine=storeModel.objects.all()
+     return render(request,'viewall.html',{'medicine':medicine})
+
+def update(request,num):
+    medid=storeModel.objects.get(pk=num)
+    if request.method=='POST':
+        form=storeForm(request.POST,instance=medid)
+        if form.is_valid():
+            form.save()
+            return redirect('viewall')
+    else:
+        form=storeForm(instance=medid)
+    return render(request,'update.html',{'form':form})
+
+def deletemed(request,num):
+     medid=storeModel.objects.get(pk=num)
+     if request.method=='POST':
+          storeModel.delete(medid)
+          return redirect('viewall')
+     return render(request,'delete.html',{'medid':medid})
--- medstoreproject/medstoreproject/urls.py
+++ medstoreproject/medstoreproject/urls.py
@@ -6,11 +6,14 @@ from django.conf import settings
 
 urlpatterns = [
     path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
+    path('login/',views.userlogin,name='login'),
+    path('logout/',views.userlogout,name='logout'),
+    path('home/',views.home,name='home'),
+    path('about-us/',views.aboutus,name='aboutus'),
     path('create/',views.create,name='create'),
-    path('signup/',views.signup,name='signup'),
-    path('signup/',views.signup,name='signup'),
-
+    path('view/',views.viewall,name='viewall'),
+    path('update/<int:num>',views.update,name='update'),
+    path('delete/<int:num>',views.deletemed,name='delete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
